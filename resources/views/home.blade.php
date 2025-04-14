19d5880ea35069dd3cd83452180079e30107477e
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2025-04-14 15:26:26 -0500

    cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -76,13 +76,13 @@
                     <div class="imagen actual">
                       <img class="pc" style="border-radius: 20px;" src="/storage/{{$item->banner_url}}" />
                       <img class="mobil" style="border-radius: 20px;display: none;" src="/storage/{{$item->banner_mobil_url}}" />
-                      <div class="texto">{{$item->name}} </div>
+                      <!-- <div class="texto">{{$item->name}} </div> -->
                     </div>
                     @else
                     <div class="imagen">
                       <img class="pc" style="border-radius: 20px;" src="/storage/{{$item->banner_url}}" />
                       <img class="mobil" style="border-radius: 20px;display: none;" src="/storage/{{$item->mobil_url}}" />
-                      <div class="texto">{{$item->name}}</div>
+                      <!-- <div class="texto">{{$item->name}}</div> -->
                     </div>
                     @endif
 
